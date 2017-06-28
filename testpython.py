2a07a04362996cc610a5a79394ac045c0539b71f
Alexandre R Franco <eng.franco@gmail.com>
2017-06-28 19:44:08 -0300

    added one more line again

--- testpython.py
+++ testpython.py
@@ -4,12 +4,22 @@
 print('hello world ')
 
 import numpy
+import subprocess
+import os
+
+
 
 x=5
 
 list_subjs = ['RWM003','RWM004']
 list_visits = ['visit1','visit2']
 print(list_subjs)
+print(list_visits)
+
+
+for ii in len(list_subjs)
+    csh_command = 'source ./setup.csh && ./script.csh'
+    subprocess.check_call(['/bin/csh', '-c', csh_command])
 
 
 
